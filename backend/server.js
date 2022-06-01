667c6663509248175e6e6064cd6a2354a182e5fa
nikballal <nikhil.ballal1994@gmail.com>
2022-06-01 13:39:11 +1000

    add routes and error handling

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Server is running....");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const PORT = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Support Desk API" });
+});
+
+//Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
